0ecaa46a41d127b7c2bc345e34215868a96ed53c
Priyanka Katore <priyankakatore819@gmail.com>
2026-04-22 10:58:00 +0530

    Enable embedded router mode for landing page builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, HashRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -20,6 +20,7 @@ import EpsonWorkforceSpotlight from "./pages/EpsonWorkforceSpotlight.tsx";
 import InsightArticle from "./pages/InsightArticle.tsx";
 
 const queryClient = new QueryClient();
+const Router = import.meta.env.VITE_EMBEDDED_LANDING_PAGE === "true" ? HashRouter : BrowserRouter;
 
 const ScrollToTop = () => {
   const { pathname, hash } = useLocation();
@@ -50,7 +51,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
@@ -81,7 +82,7 @@ const App = () => (
           <Route path="/contact.html" element={<Sales />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
